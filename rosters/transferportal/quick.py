3de1d53b0aebe602e2cfe6fe9f667f520a43c162
rubenxrodriguez <rrodr102@lion.lmu.edu>
2026-01-01 22:48:09 -0800

    Delete rosters directory

--- rosters/transferportal/quick.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-df = pd.read_csv("transfer_portal.csv")
-denver = df[df['teamMarketTo']=='Denver']
-
-cols = ['fullName','position','teamMarketFrom','mins','ptsScored','ast','reb']
-print('\n','*'*75)
-print(denver[cols])
